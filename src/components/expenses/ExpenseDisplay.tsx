e7098d5e91ec17f8a4992c986a86ea6901d07f46
Devon Wolfkiel <devonwolfkiel@LT-APL0085676.local>
2021-09-08 14:39:41 -0700

    Add year filter and state

--- src/components/expenses/ExpenseDisplay.tsx
+++ src/components/expenses/ExpenseDisplay.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseChart from './ExpenseChart';
 import ExpenseList, { ExpenseListProps } from './ExpenseList';
 import './styles/ExpenseDisplay.css';
 import Layout from '../UI/Layout';
 
 const ExpenseDisplay = ({ expenses }: ExpenseListProps): JSX.Element => {
+    const [year, setYear] = useState('2021');
+
+    console.log(year);
+
     return (
-        <Layout className="expenseDisplay">
-            <ExpenseChart />
-            <ExpenseList expenses={expenses}/>
-        </Layout>
+        <>
+            <div>
+                <label>Filter by year</label>
+                <select
+                    value={year}
+                    onChange={e => setYear(e.target.value)}
+                >
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
+                </select>
+            </div>
+            <Layout className="expenseDisplay">
+                <ExpenseChart />
+                <ExpenseList expenses={expenses}/>
+            </Layout>
+        </>
     );
 };
 
