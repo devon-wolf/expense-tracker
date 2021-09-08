cdcf06e6771bd1b4247a1083fb89eb05a0a0e692
Devon Wolfkiel <devonwolfkiel@LT-APL0085676.local>
2021-09-08 13:02:11 -0700

    Add initial form state

--- src/components/expenses/ExpenseForm.tsx
+++ src/components/expenses/ExpenseForm.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import Card from '../UI/Card';
 import './styles/ExpenseForm.css';
 
+
 const ExpenseForm = (): JSX.Element => {
+    const [dateString, setDateString] = useState<string | null>(null);
+    const [title, setTitle] = useState<string | null>(null);
+    const [amountString, setAmountString] = useState<string | null>(null);
+
+    const handleExpenseForm = (e: FormEvent): void => {
+        e.preventDefault();
+        // create a new expense item for the new expense
+        console.log(dateString, title, amountString);
+        setDateString(null);
+        setTitle(null);
+        setAmountString(null);
+    };
+
     return (
         <Card>
-            <form className="expenseForm">
+            <form 
+                className="expenseForm"
+                onSubmit={handleExpenseForm}
+            >
                 <label>
                     <span>Date</span>
-                    <input type="date" />
+                    <input
+                        type="date"
+                        value={dateString || ''}
+                        onChange={e => setDateString(e.target.value)} 
+                    />
                 </label>
 
                 <label>
                     <span>Description</span>
-                    <input type="text"/>
+                    <input
+                        type="text"
+                        value={title || ''}
+                        onChange={e => setTitle(e.target.value)}
+                    />
                 </label>
 
                 <label>
                     <span>Amount</span>
-                    <input type="number"/>
+                    <input
+                        type="number"
+                        value={amountString || ''}
+                        onChange={e => setAmountString(e.target.value)}
+                    />
                 </label>
             
                 <button>Add Expense</button>
